1ae63781d726bb3ac40922719a8bad0e8b9c0eca
marslinoED <lino.edward11@gmail.com>
2025-12-06 21:52:19 +0200

    Commit : 378

--- Node_Project/01_ToursApi/routes/documentationRoute.js
+++ Node_Project/01_ToursApi/routes/documentationRoute.js
@@ -7,12 +7,12 @@ router.get("/", (req, res) => {
     message: "Welcome to Tours API",
     try: "Use /api/v1/tours to access tours resources",
     go_to: "Use /api-docs for API documentation",
-    docs: "https://documenter.getpostman.com/view/XXXXXXX",
+    docs: "https://documenter.getpostman.com/view/48419535/2sB3Wqv1ZB",
   });
 });
 
 router.get("/api-docs", (req, res) => {
-  res.redirect("https://documenter.getpostman.com/view/XXXXXXX");
+  res.redirect("https://documenter.getpostman.com/view/48419535/2sB3Wqv1ZB");
 });
 
 module.exports = router;
