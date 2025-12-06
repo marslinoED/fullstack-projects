7fc439afdb372940cd40af087588a53c124e8616
marslinoED <lino.edward11@gmail.com>
2025-12-06 19:49:28 +0200

    Commit : 374

--- Node_Project/Express/api/index.js
+++ Node_Project/Express/api/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(morgan('dev'));
@@ -10,16 +9,17 @@ app.use(morgan('dev'));
 app.get('/', (req, res) => {
   res.send('Hello, Express! GET');
 });
+
 app.post('/', (req, res) => {
   res.send('Hello, Express! POST');
 });
+
 app.patch('/', (req, res) => {
   res.send('Hello, Express! PATCH');
 });
+
 app.delete('/', (req, res) => {
   res.send('Hello, Express! DELETE');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
