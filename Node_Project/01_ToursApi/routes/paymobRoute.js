8a32b854e042d01b488c5bae258764e52094f932
marslinoED <lino.edward11@gmail.com>
2025-12-13 21:06:52 +0200

    Commit : 417

--- Node_Project/01_ToursApi/routes/paymobRoute.js
+++ Node_Project/01_ToursApi/routes/paymobRoute.js
@@ -2,9 +2,16 @@ const express = require("express");
 
 const router = express.Router({ mergeParams: true });
 
-router.get("/webhook", express.json(), (req, res) => {
+router.post("/webhook", express.json(), (req, res) => {
   console.log(req.body);
   res.sendStatus(200);
+});
+router.get("/webhook", express.json(), (req, res) => {
+  res.status(201).json({
+    status: "success",
+    data: "Paymob webhook is set up successfully",
+    body: req.body,
+  });
 });
 
 module.exports = router;
